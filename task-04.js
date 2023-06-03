d958ec154287a74fff1061bacd50a12f2f4b72cd
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-06-03 13:10:30 +0200

    іі

---
+++ task-04.js
@@ -0,0 +1,15 @@
+const counterValue = document.getElementById('value');
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]');
+
+let count = 0;
+
+decrementButton.addEventListener('click', () => {
+  count -= 1;
+  counterValue.textContent = count;
+});
+
+incrementButton.addEventListener('click', () => {
+  count += 1;
+  counterValue.textContent = count;
+});
