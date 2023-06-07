7daff493d62a87cb7518a0b16c108c85131ebf18
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-06-07 23:24:30 +0200

    plizzzzz

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const fontSizeControl = document.getElementById('font-size-control');
 const textSpan = document.getElementById('text');
-
+textSpan.style.fontSize = fontSizeControl.value + 'px';
 fontSizeControl.addEventListener('input', function () {
   const fontSize = fontSizeControl.value + 'px';
   textSpan.style.fontSize = fontSize;
