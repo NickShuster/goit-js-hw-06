88f11e4ee8bb1413dc80456f0ef27e3f6fe59dc5
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-06-08 11:43:15 +0200

    cc

--- js/task-03.js
+++ js/task-03.js
@@ -1,4 +1,42 @@
-      const images = [
+      // const images = [
+      //   {
+      //     url:
+      //       "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+      //     alt: "White and Black Long Fur Cat",
+      //   },
+      //   {
+      //     url:
+      //       "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+      //     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+      //   },
+      //   {
+      //     url:
+      //       "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+      //     alt: "Group of Horses Running",
+      //   },
+      // ];
+
+      // const gallery = document.querySelector(".gallery");
+
+      // const galleryItems = images.map((image) => {
+      //   const li = document.createElement("li");
+      //   li.classList.add("gallery__item");
+
+      //   const img = document.createElement("img");
+      //   img.src = image.url;
+      //   img.alt = image.alt;
+      //   img.width = "300";
+      //   img.height = "200";
+
+      //   li.appendChild(img);
+      //   return li;
+      // });
+
+      // galleryItems.forEach((item) => {
+      //   gallery.appendChild(item);
+      // });
+      
+            const images = [
         {
           url:
             "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -18,20 +56,13 @@
 
       const gallery = document.querySelector(".gallery");
 
-      const galleryItems = images.map((image) => {
-        const li = document.createElement("li");
-        li.classList.add("gallery__item");
+      const galleryItems = images.map(
+        (image) => `
+         <li class="gallery-item">
+         <img src="${image.url}" alt="${image.alt}" class="gallery-img" style="width: 300px; height: 200px;">
+          </li>
+        `
+      );
 
-        const img = document.createElement("img");
-        img.src = image.url;
-        img.alt = image.alt;
-        img.width = "300";
-        img.height = "200";
-
-        li.appendChild(img);
-        return li;
-      });
-
-      galleryItems.forEach((item) => {
-        gallery.appendChild(item);
-      });
+gallery.insertAdjacentHTML("beforeend", galleryItems.join(""));
+      
